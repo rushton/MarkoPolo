7162fa70c9b63c4bd867c860d699ef0f16898faa
Nicholas Rushton <nick.rushton@hotmail.com>
2013-09-21 09:40:05 -0700

    init angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('markopoloApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
