9d48af39d055d9621e8ef6f75edaaae669c62671
Nicholas Rushton <nick.rushton@hotmail.com>
2013-09-21 16:01:26 -0700

    added find nearby function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,22 @@
 'use strict';
 
 angular.module('markopoloApp')
-  .controller('MainCtrl', function ($scope, Facebook, Geolocation, Locationposter) {
+  .controller('MainCtrl', function ($scope, Facebook, Geolocation) {
     $scope.user = Facebook.getUser(FB);
     Geolocation.getLocation(function(place){
        $scope.location = place;
-       Locationposter.save({lat: place.coords.latitude, long: place.coords.longitude, user_id: 0})
-       $scope.$apply()
+
+       var friendPromise = Facebook.getFriends(FB);
+       var fbusers = [{location:{name:'Ballard, WA'}}].filter(function(v,k) {
+          return v.location && v.location.name;
+       });
+       Geolocation.findNearby({lat:place.coords.latitude, long:place.coords.longitude},fbusers, 5).then(function(d) { 
+          console.log(d);  
+       });
+       friendPromise.then(function(data) {
+         $scope.friends = data.data;
+         //Geolocation.findNearby(place,data.data);
+       });
     })
 
-    var friendPromise = Facebook.getFriends(FB);
-    friendPromise.then(function(data) {
-      $scope.friends = data.data;
-    });
   });
--- app/scripts/services/geolocation.js
+++ app/scripts/services/geolocation.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('markopoloApp')
-  .service('Geolocation', function Geolocation() {
+  .service('Geolocation', function Geolocation($q) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     var geocoder = new google.maps.Geocoder();
     function getExtra(lat,long, callback) {
@@ -21,17 +21,50 @@ angular.module('markopoloApp')
             callback(final)
          })
     }
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = deg2rad(lon2-lon1); 
+      var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c; // Distance in km
+      return d;
+    }
+ 
+    function deg2rad(deg) {
+      return deg * (Math.PI/180)
+    }
+    function getDistanceFromLatLonInMiles(lat1,lon1,lat2,lon2) {
+       return getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) * 0.62137;
+    }
+    function fromCity(fbUser) {
+         var deferred = $q.defer();
+         geocoder.geocode({'address': fbUser.location.name}, function(res) {
+            deferred.resolve({lat: res[0].geometry.location.ob, long: res[0].geometry.location.pb, user: fbUser})
+         })
+         return deferred.promise;
+    }
     return {
        getLocation: function(callback) {
          navigator.geolocation.getCurrentPosition(function(pos) {
             getExtra(pos.coords.latitude,pos.coords.longitude,callback);  
        })
       },
-      fromCity: function(city, state, callback) {
-         console.log(geocoder)
-         geocoder.geocode({'address': [city, state].join(', ')}, function(res) {
-            callback({lat: res[0].geometry.location.ob, long: res[0].geometry.location.pb})
-         })
+      fromCity: fromCity,
+      findNearby: function(source,targets, range) {
+         var deferred = $q.defer();
+         $q.all(targets.map(function(v,k) {
+            return fromCity(v)
+         })).then(function(data){
+               var v = data.filter(function(v,k) { 
+                  return range > getDistanceFromLatLonInMiles(v.lat,v.long,source.lat,source.long) })
+               deferred.resolve(v.map(function(v,k) { return v.user }));
+         });
+         return deferred.promise;
       }
     }
   });
