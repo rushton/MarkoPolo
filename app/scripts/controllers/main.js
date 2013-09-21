8e2643048751a96a9f0f5ca0f35e014173595fe2
Jay Zeng <jayzeng@jay-zeng.com>
2013-09-21 12:18:18 -0700

    * Facebook login

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('markopoloApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, Facebook) {
+    $scope.user = Facebook.getUser(FB);
   });
